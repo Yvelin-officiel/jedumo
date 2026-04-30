42a06ceb27f07082c90f0786c25757fcfaff84fb
Yvelin-officiel <gerbeaudy@yahoo.fr>
2026-04-30 13:41:18 +0200

    fix/hydration

--- src/app/jedumo/layout.tsx
+++ src/app/jedumo/layout.tsx
@@ -7,12 +7,12 @@ export const metadata: Metadata = {
 
 export default function JeudumoLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode
-}>) {
+}) {
   return (
-    <html lang="en" className="h-full ">
-      <body className="min-h-full flex flex-col">{children}</body>
-    </html>
+    <div className="min-h-full flex flex-col">
+      {children}
+    </div>
   )
 }
